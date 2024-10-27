33668dd98fbcc33be7169e36b1cbd0de432fe3a1
Caner Türe <canert@avvamobile.com>
2024-10-27 17:58:07 +0300

    increase jvm version

--- mlinkmobile/build.gradle.kts
+++ mlinkmobile/build.gradle.kts
@@ -35,11 +35,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -68,7 +68,7 @@ afterEvaluate {
         publishing {
             publications {
                 register<MavenPublication>(it.name) {
-                    from(components.findByName(it.name))
+                    from(components.findByName("release"))
 
                     groupId = "com.mimeda.mlink"
                     artifactId = "android"
